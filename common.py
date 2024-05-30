6ba492d6f8c347a24f9677b416b2ee3789ef1ba5
gavboi <gj1087@hotmail.com>
2024-05-30 16:17:22 -0400

    added index slash letter converter function

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 def get_all_words() -> List[str]:
     with open('five_letter_words.txt', 'r') as file:
@@ -28,3 +28,13 @@ def validate_guess(guess: str, target_word: str) -> Tuple[List[str]]:
         else:
             wrong.append(letter)
     return correct, close, wrong
+
+def convert(arg: Union[str, int]) -> Union[str, int]:
+    if type(arg) is str:
+        if len(arg) > 1:
+            raise ValueError(f'Arg {arg} is not length 1')
+        return ord(arg.lower()) - ord('a')
+    elif type(arg) is int:
+        if int >= 26 or int < 0:
+            raise ValueError(f'Arg {arg} must be within the length of alphabet')
+        return chr(arg + ord('a'))
