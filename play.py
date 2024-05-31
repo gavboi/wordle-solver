5db7a2f18b3e0e4c6cc930aa7868e4cff4092d8c
gavboi <gj1087@hotmail.com>
2024-05-31 16:29:50 -0400

    added main function

--- play.py
+++ play.py
@@ -3,27 +3,32 @@ from common import get_all_words, validate_guess
 import random
 import sys
 
-# parse options
-guess_count = int(sys.argv[1]) if len(sys.argv) > 1 else 6
 
-# read words
-all_words = get_all_words()
+def main():
+    # parse options
+    guess_count = int(sys.argv[1]) if len(sys.argv) > 1 else 6
 
-# pick word
-target_word = random.choice(all_words)
+    # read words
+    all_words = get_all_words()
 
-# play
-for g in range(guess_count):
-    guess = ''
-    while len(guess) != 5:
-        guess = input(f'Guess {g+1}: ')
-    correct, close, wrong = validate_guess(guess, target_word)
-    if len(correct) == 5:
-        print('Yeehaw')
-        break
-    print(f'Correct: {",".join(correct)}')
-    print(f'Close: {",".join(close)}')
-    print(f'Wrong: {",".join(wrong)}')
-    print('')
+    # pick word
+    target_word = random.choice(all_words)
 
-print(f'The word was: {target_word}')
+    # play
+    for g in range(guess_count):
+        guess = ''
+        while len(guess) != 5:
+            guess = input(f'Guess {g+1}: ')
+        correct, close, wrong = validate_guess(guess, target_word)
+        if len(correct) == 5:
+            print('Yeehaw')
+            break
+        print(f'Correct: {",".join(correct)}')
+        print(f'Close: {",".join(close)}')
+        print(f'Wrong: {",".join(wrong)}')
+        print('')
+
+    print(f'The word was: {target_word}')
+
+if __name__ == '__main__':
+    main()
